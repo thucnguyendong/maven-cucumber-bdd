369ccaf7af9769856dbd1c61a2a4da35a78bdd37
Nguyen Dong Thuc <nguyendongthuc13@gmail.com>
2022-06-24 00:34:43 +0700

    Create maven-cucumber-bdd framework

---
+++ src/test/java/stepDefinitions/facebookSteps.java
@@ -0,0 +1,43 @@
+package stepDefinitions;
+
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class facebookSteps {
+	WebDriver driver;
+	
+	@Given("^Open facebook application$")
+	public void openFacebookApplication() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+	}
+
+	@Then("^Verify email textbox is displayed$")
+	public void verifyEmailTextboxIsDisplayed() {
+		Assert.assertTrue(driver.findElement(By.id("email")).isDisplayed());
+	}
+
+	@And("^Verify password textbox is displayed$")
+	public void verifyPasswordTextboxIsDisplayed() {
+		Assert.assertTrue(driver.findElement(By.id("pass")).isDisplayed());		
+	}
+	
+	@And("^Close application$")
+	public void closeApplication() {
+		driver.quit();		
+	}
+
+}
